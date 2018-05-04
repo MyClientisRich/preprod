a5f9509e44758c4b7ab388c0a6f6b6ec809075d3
Thomas Moreira <thomas@myclientisrich.com>
2018-05-04 10:17:10 +0200

    preprod

---
+++ preprod.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+base_folder="~/"
+preprod_folder=${base_folder}"preprod"
+alias_file=${base_folder}".bash_profile"
+
+if [ -d "$preprod_folder" ]; then
+    echo "---------------------------------"
+    echo "/!\ Attention, le dossier $preprod_folder est déjà présent sur ce serveur !"
+    echo "/!\ Il sera écrasé."
+    echo "---------------------------------"
+
+    echo -n "On continue ? (y/N) : "
+    read response
+
+    case "$response" in
+    "y" | "Y" | "yes" | "YES" )
+        echo "Let's go !"
+        rm -rf ${preprod_folder}
+        echo "Dossier $preprod_folder supprimé."
+        ;;
+    *)
+        echo "On arrête là !"
+        exit 1;
+    esac
+fi
+
+if [ $# -lt 1 ]; then
+    # ----------- On demande le nom du projet (nom du dossier)
+    echo -n "Entrez le nom du projet (au même format que dans l'url bitbucket)  : "
+    read projectname
+else
+    projectname=$1
+fi
+
+while [ "$projectname" = "" ]; do
+    echo -n "Il me faut un nom de projet ! : "
+    read projectname
+done
+
+echo "Clone du dépôt GIT :"
+git clone https://mcir@bitbucket.org/mcir/${projectname}.git ${preprod_folder}
+echo "Dossier cloné dans $preprod_folder"
+
+echo "=================================="
+
+echo "Téléchargement des outils :"
+echo "1) Composer"
+wget https://files.myclientisrich.com/composer_wp/composer.phar ${base_folder}
+echo "2) Wp-cli"
+wget https://files.myclientisrich.com/composer_wp/wp-cli.phar ${base_folder}
+
+echo "=================================="
+echo "On prépare les alias vers les fichiers"
+echo 'alias composer="'${base_folder}'composer.phar"' >> ${alias_file}
+echo 'alias wp="'${base_folder}'wp-cli.phar"' >> ${alias_file}
+source ${alias_file}
+
+echo "=================================="
+echo "On rend les 2 fichiers executables"
+chmod +x ${base_folder}composer.phar
+chmod +x ${base_folder}wp-cli.phar
+
+echo "=================================="
+echo "On met à jour les 2 outils"
+${base_folder}composer.phar self-update
+${base_folder}wp-cli.phar cli update
+
+echo "Et c'est pret !"
